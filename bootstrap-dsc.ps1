e3d760e61df649bf066f2fc440692bba6cb248fe
Mark Young <myoung808@gmail.com>
2021-07-22 12:58:56 -1000

    update import-dscresource

--- bootstrap-dsc.ps1
+++ bootstrap-dsc.ps1
@@ -1,7 +1,5 @@
 Configuration Bootstrap {
-    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
-
-    Import-DscResource -ModuleName PSDesiredStateConfiguration -Name Registry
+    Import-DscResource -ModuleName PSDesiredStateConfiguration -Name Registry, Script
 
     Node localhost {
         Registry EnableRDP {
@@ -15,7 +13,13 @@ Configuration Bootstrap {
         Script EnableRDPFirewallRule {
             SetScript = { Enable-NetFirewallRule -Name RemoteDesktop-UserMode-In-TCP }
             TestScript = { ( Get-NetFirewallRule -Name RemoteDesktop-UserMode-In-TCP ).Enabled }
-            GetScript = { @{ Result = (Get-NetFirewallRule -Name RemoteDesktop-UserMode-In-TCP).Enabled } }
+            GetScript = { @{ Result = ( Get-NetFirewallRule -Name RemoteDesktop-UserMode-In-TCP ).Enabled } }
+        }
+
+        Script EnableICMPFirewallRule {
+            SetScript = { Enable-NetFirewallRule -Name vm-monitoring-icmpv4 }
+            TestScript = { ( Get-NetFirewallRule -Name vm-monitoring-icmpv4 ).Enabled }
+            GetScript = { @{ Result = ( Get-NetFirewallRule -Name vm-monitoring-icmpv4 ).Enabled } }
         }
     }
 }
