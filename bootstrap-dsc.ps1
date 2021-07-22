fec68c239ab51a55751536da012d489a70969c11
Mark Young <myoung808@gmail.com>
2021-07-22 12:34:35 -1000

    added resource to enable RDP firewall rule

--- bootstrap-dsc.ps1
+++ bootstrap-dsc.ps1
@@ -1,12 +1,22 @@
-Configuration EnableRDP {
+Configuration Bootstrap {
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+
+    Import-DscResource -ModuleName PSDesiredStateConfiguration -Name Registry
+
     Node localhost {
-        Registry fDenyTSConnections {
+        Registry EnableRDP {
             Ensure = "Present"
-            Key = "HKLM:\SYSTEM\CurentControlSet\Control\Terminal Server"
+            Key = "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server"
             ValueName = "fDenyTSConnections"
             ValueData = "0"
             ValueType = "Dword"
         }
+
+        Script EnableRDPFirewallRule {
+            SetScript = { Enable-NetFirewallRule -Name RemoteDesktop-UserMode-In-TCP }
+            TestScript = { ( Get-NetFirewallRule -Name RemoteDesktop-UserMode-In-TCP ).Enabled }
+            GetScript = { @{ Result = (Get-NetFirewallRule -Name RemoteDesktop-UserMode-In-TCP).Enabled } }
+        }
     }
 }
-EnableRDP
+Bootstrap
