056938b0e79e626d35d8b3e48113f436769d263c
Mark Young <myoung808@gmail.com>
2021-07-23 08:02:00 -1000

    bootstrap-dsc.ps1: fix whitespace

--- bootstrap-dsc.ps1
+++ bootstrap-dsc.ps1
@@ -12,14 +12,14 @@ Configuration Bootstrap {
 
         Script EnableRDPFirewallRule {
             SetScript = { Enable-NetFirewallRule -Name RemoteDesktop-UserMode-In-TCP }
-            TestScript = { ( Get-NetFirewallRule -Name RemoteDesktop-UserMode-In-TCP ).Enabled }
-            GetScript = { @{ Result = ( Get-NetFirewallRule -Name RemoteDesktop-UserMode-In-TCP ).Enabled } }
+            TestScript = { (Get-NetFirewallRule -Name RemoteDesktop-UserMode-In-TCP).Enabled }
+            GetScript = { @{ Result = (Get-NetFirewallRule -Name RemoteDesktop-UserMode-In-TCP).Enabled } }
         }
 
         Script EnableICMPFirewallRule {
             SetScript = { Enable-NetFirewallRule -Name vm-monitoring-icmpv4 }
-            TestScript = { ( Get-NetFirewallRule -Name vm-monitoring-icmpv4 ).Enabled }
-            GetScript = { @{ Result = ( Get-NetFirewallRule -Name vm-monitoring-icmpv4 ).Enabled } }
+            TestScript = { (Get-NetFirewallRule -Name vm-monitoring-icmpv4).Enabled }
+            GetScript = { @{ Result = (Get-NetFirewallRule -Name vm-monitoring-icmpv4).Enabled } }
         }
     }
 }
