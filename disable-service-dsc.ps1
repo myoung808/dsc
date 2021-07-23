458c7b8a5a351b1d2bda391104121ac6b0bb7409
Mark Young <myoung808@gmail.com>
2021-07-22 16:55:12 -1000

    switch to serviceset

--- disable-service-dsc.ps1
+++ disable-service-dsc.ps1
@@ -1,72 +1,16 @@
 # Based on https://docs.microsoft.com/en-us/windows-server/security/windows-services/security-guidelines-for-disabling-system-services-in-windows-server
-
-$ConfigurationData = @{
-    AllNodes =
-    @(
-        @{
-           NodeName = 'localhost'
-           Services = @(
-               @{ Name = "AxInstSV"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "bthserv"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "CDPUserSvc"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "PimIndexMaintenanceSvc"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "dmwappushservice"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "MapsBroker"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "lfsvc"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "SharedAccess"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "lltdsvc"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "wlidsvc"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "NgcSvc"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "NgcCtnrSvc"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "NcbService"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "PhoneSvc"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "Spooler"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "PrintNotify"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "PcaSvc"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "QWAVE"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "RmSvc"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "SensorDataService"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "SensrSvc"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "SensorService"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "ShellHWDetection"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "ScDeviceEnum"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "SSDPSRV"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "WiaRpc"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               #@{ Name = "OneSyncSvc"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "TabletInputService"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "upnphost"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "UserDataSvc"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "UnistoreSvc"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "WalletService"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "Audiosrv"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "AudioEndpointBuilder"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "FrameServer"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "stisvc"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "wisvc"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "icssvc"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "WpnService"; StartupType = "Disabled"; State = "Stopped" }, # OK to disable
-               @{ Name = "WpnUserService"; StartupType = "Disabled"; State = "Stopped" } # OK to disable
-               #@{ Name = "XblAuthManager"; StartupType = "Disabled"; State = "Stopped" }, # Should disable
-               #@{ Name = "XblGameSave"; StartupType = "Disabled"; State = "Stopped" } # Should disable
-           )
-        }
-    )
-}
+# "OneSyncSvc", "XblAuthManager", "XblGameSave" don't seem to exist in Windows Server 2019
 
 Configuration disable-service {
-    Import-DscResource -ModuleName PSDesiredStateConfiguration -Name Service
+    Import-DscResource -ModuleName PSDesiredStateConfiguration -Name ServiceSet
 
     Node localhost {
-        foreach ($s in $Node.Services)
-        {
-            service $s.Name
-            {
-                Name = $s.Name;
-                StartupType = $s.StartType;
-                State = $s.State
+        ServiceSet DisableService {
+            Name = @( "AxInstSV", "bthserv", "CDPUserSvc", "PimIndexMaintenanceSvc", "dmwappushservice", "MapsBroker", "lfsvc", "SharedAccess", "lltdsvc", "wlidsvc", "NgcSvc", "NgcCtnrSvc", "NcbService", "PhoneSvc", "Spooler", "PrintNotify", "PcaSvc", "QWAVE", "RmSvc", "SensorDataService", "SensrSvc", "SensorService", "ShellHWDetection", "ScDeviceEnum", "SSDPSRV", "WiaRpc", "TabletInputService", "upnphost", "UserDataSvc", "UnistoreSvc", "WalletService", "Audiosrv", "AudioEndpointBuilder", "FrameServer", "stisvc", "wisvc", "icssvc", "WpnService", "WpnUserService" )
+            StartupType = "Disabled"
+            State = "Stopped"
 
-            }
         }
     }
 }
-disable-service -ConfigurationData $ConfigurationData
+disable-service
