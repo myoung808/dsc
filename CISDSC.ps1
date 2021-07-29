b4c5ee727a29106ec6b1b7c0b10d8d4b3e38ad11
Mark Young <myoung808@gmail.com>
2021-07-28 15:11:23 -1000

    whitespace

--- CISDSC.ps1
+++ CISDSC.ps1
@@ -3,7 +3,12 @@ Configuration CISDSC {
 
     Node localhost {
         CIS_Microsoft_Windows_Server_2019_Member_Server_Release_1809 CISLevel1 {
-            ExcludeList = @("1.1.2", "1.1.3", "18.2.5", "18.2.6", "2.3.1.5", "2.3.1.6", "2.3.6.5", "2.3.7.4", "2.3.7.5")
+            ExcludeList = @(
+                "2.3.1.5",
+                "2.3.1.6",
+                "2.3.7.4",
+                "2.3.7.5"
+            )
         }
     }
 }
